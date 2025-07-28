e18569437e79ee842a7ac403687d16d3a1dafa29
Deeksha Chauhan <deeksha7chauhan@gmail.com>
2025-07-28 22:35:24 +0530

    Update EX-27.sql

--- sql-exru/EX-27.sql
+++ sql-exru/EX-27.sql
@@ -1,1 +1,19 @@
+### Find out the average hard disk drive capacity of PCs produced by makers who also manufacture printers.
+Result set: maker, average HDD capacity.
+
+SELECT 
+    p1.maker, 
+    AVG(pc.hd) AS average_hdd
+FROM 
+    Product p1
+JOIN 
+    PC pc ON p1.model = pc.model
+WHERE 
+    p1.maker IN (
+        SELECT DISTINCT p2.maker
+        FROM Product p2
+        WHERE p2.type = 'Printer'
+    )
+GROUP BY 
+    p1.maker;
 
